75af7e769981c3ed95cc8d92074e39be8457a49c
Foudroyant <stephanebazebibouta@gmail.com>
2024-06-01 10:22:34 +0100

    Premier commit kenn service

---
+++ app/components/header.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-white shadow">
+      <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+        <div className="text-3xl font-bold">Kenn Services</div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="#home" className="hover:underline">Home</a></li>
+            <li><a href="#about" className="hover:underline">About</a></li>
+            <li><a href="#services" className="hover:underline">Services</a></li>
+            <li><a href="#contact" className="hover:underline">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
